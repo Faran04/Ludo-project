de4e826b05d6f72e36d87b3468f9fbbb89694579
Faran04 <156823008+Faran04@users.noreply.github.com>
2026-03-18 13:39:48 +0000

    Add initial HTML structure for Ludo JS Game
    18-03-2026
    just a simple index which also include  a button for a dice roll this will further be editted

---
+++ index.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ludo JS Game</title>
+<link rel="stylesheet" href="style.css">
+</head>
+ 
+<body>
+ 
+<h1>Ludo Game</h1>
+ 
+<div id="board"></div>
+ 
+<button onclick="rollDice()">Roll Dice</button>
+ 
+<p id="diceResult">Dice: </p>
+<p id="turnInfo"></p>
+ 
+<script src="game.js"></script>
+ 
+</body>
+</html>
